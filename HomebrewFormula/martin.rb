3dd9a613dd52cf45157fb7036231d3a9f3de0e98
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-10-10 19:37:02 +0300

    update martin formula

--- HomebrewFormula/martin.rb
+++ HomebrewFormula/martin.rb
@@ -3,14 +3,13 @@ class Martin < Formula
   desc "PostGIS Mapbox Vector Tiles server"
   homepage "https://github.com/urbica/martin"
   url "https://github.com/urbica/martin/releases/download/v#{current_version}/martin-darwin-x86_64.zip"
-  sha256 "fc7dac7da8d2773ab033091b8c3321271c77e2a54014a09ae0de284089c02763"
+  sha256 "f7eb6871d61c0a20c91aef33a03ca712ada34b4e5eaf88e2cab74491496abbd1"
 
   def install
     bin.install "martin"
   end
 
   test do
-    system "false"
-    # `#{bin}/martin --version`
+    `#{bin}/martin --version`
   end
 end
