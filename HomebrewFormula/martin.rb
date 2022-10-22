a034f622bcdef22515596f2c3b38cae6fff74f3d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-10-22 20:57:27 +0300

    bump martin to 0.6.0-rc.3

--- HomebrewFormula/martin.rb
+++ HomebrewFormula/martin.rb
@@ -1,10 +1,10 @@
 class Martin < Formula
-  current_version="0.5.0"
+  current_version="0.6.0-rc.3"
 
   desc "PostGIS Mapbox Vector Tiles server"
-  homepage "https://github.com/urbica/martin"
-  url "https://github.com/urbica/martin/releases/download/v#{current_version}/martin-Darwin-x86_64.tar.gz"
-  sha256 "b80a97912e1c084e878dd89e979bfbd2525f8db95efa67b33a0c5b3d34783f29"
+  homepage "https://github.com/maplibre/martin"
+  url "https://github.com/maplibre/martin/releases/download/v#{current_version}/martin-Darwin-x86_64.tar.gz"
+  sha256 "c7e98e78997698d7d29e7cd8c4c33ad31f0504a275a5f5bc36c40fae5c5cc965"
   version "#{current_version}"
 
   def install
